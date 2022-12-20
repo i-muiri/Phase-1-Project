9b1753472d60c9e71210066f2f19b84afce43a7b
i-muiri <ianmuiri99@gmail.com>
2022-12-20 14:13:56 +0300

    Added setCocktailDetails function

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const drinkName = document.getElementById("name")
+const category = document.getElementById("category")
+const glass = document.getElementById("glass")
+const image = document.getElementById("image")
+const instructions = document.getElementById("instructions")
+
+
 let cocktails=[];
 
 window.addEventListener('load', () => {
@@ -24,9 +31,24 @@ function listCocktails(){
             const listItem = document.createElement("li");
             listItem.textContent =cocktail.strDrink;
             listItem.setAttribute("class", "listItem");
-
+            
+            
+            //add <li> element to <ul>
             list.append(listItem)  
         })
         
     }
+}
+
+function setCocktailDetails(){
+    //checking each item id is equal selected cocktailID
+    const cocktail = cocktails.find((c) => c.idDrink == selectedCocktailId); 
+
+    drinkName.textContent = cocktail.strDrink;
+    category.textContent =cocktail.strCategory;
+    glass.textContent = cocktail.strGlass;
+    image.setAttribute("src", cocktail.strDrinkThumb);
+    instructions.textContent = cocktail.strInstructions;
+
+
 }
